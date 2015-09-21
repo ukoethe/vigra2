71ec0de745e9c7a9ecf7fff51ba514bf30b082bc
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-09-21 17:32:47 +0200

    added axistags.hxx and test

---
+++ include/vigra2/axistags.hxx
@@ -0,0 +1,334 @@
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 2014-2015 by Ullrich Koethe                  */
+/*                                                                      */
+/*    This file is part of the VIGRA2 computer vision library.          */
+/*    The VIGRA2 Website is                                             */
+/*        http://ukoethe.github.io/vigra2                               */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        ullrich.koethe@iwr.uni-heidelberg.de    or                    */
+/*        vigra@informatik.uni-hamburg.de                               */
+/*                                                                      */
+/*    Permission is hereby granted, free of charge, to any person       */
+/*    obtaining a copy of this software and associated documentation    */
+/*    files (the "Software"), to deal in the Software without           */
+/*    restriction, including without limitation the rights to use,      */
+/*    copy, modify, merge, publish, distribute, sublicense, and/or      */
+/*    sell copies of the Software, and to permit persons to whom the    */
+/*    Software is furnished to do so, subject to the following          */
+/*    conditions:                                                       */
+/*                                                                      */
+/*    The above copyright notice and this permission notice shall be    */
+/*    included in all copies or substantial portions of the             */
+/*    Software.                                                         */
+/*                                                                      */
+/*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND    */
+/*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES   */
+/*    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND          */
+/*    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT       */
+/*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
+/*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
+/*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */
+/*                                                                      */
+/************************************************************************/
+
+#pragma once
+
+#ifndef VIGRA_AXISTAGS_HXX
+#define VIGRA_AXISTAGS_HXX
+
+#include "config.hxx"
+#include "tinyarray.hxx"
+#include <string>
+#include <iostream>
+
+namespace vigra {
+
+namespace tags {
+
+enum AxisType { Channels = 1, 
+                Space = 2, 
+                Angle = 4, 
+                Time = 8, 
+                Frequency = 16, 
+                Edge = 32,
+                UnknownAxisType = 64, 
+                NonChannel = Space | Angle | Time | Frequency | UnknownAxisType,
+                AllAxes = 2*UnknownAxisType-1 };
+
+    // tags are arranged in sorting order
+enum AxisTag  { c,  // channel axis
+                n,  // node map for a graph
+                x,  // spatial x-axis
+                y,  // spatial y-axis
+                z,  // spatial z-axis
+                t,  // time axis
+                fx, // Fourier transform of x-axis
+                fy, // Fourier transform of y-axis
+                fz, // Fourier transform of z-axis
+                ft, // Fourier transform of t-axis
+                e,  // edge map for a graph
+                unknown_axistag
+              };
+
+const char * AxisTagKeys[] = { "c",
+                               "n",
+                               "x",
+                               "y",
+                               "z",
+                               "t",
+                               "x",
+                               "y",
+                               "z",
+                               "t",
+                               "e",
+                               "?"
+                              };
+
+AxisType AxisTagTypes[] = { Channels,                    // c
+                            Space,                       // n
+                            Space,                       // x
+                            Space,                       // y
+                            Space,                       // z
+                            Time,                        // t
+                            AxisType(Space | Frequency), // fx
+                            AxisType(Space | Frequency), // fy
+                            AxisType(Space | Frequency), // fz
+                            AxisType(Time | Frequency),  // ft
+                            Edge,                        // e
+                            UnknownAxisType              // unknown_axistag
+                          };
+} // namespace tags
+
+using tags::AxisType;
+using tags::AxisTag;
+
+class AxisInfo
+{
+  public:
+  
+    AxisInfo(AxisTag tag=tags::unknown_axistag, 
+             double resolution = 0.0, std::string description = "")
+    : key_(tags::AxisTagKeys[tag]),
+      description_(description),
+      resolution_(resolution),
+      flags_(tags::AxisTagTypes[tag])
+    {}
+    
+    AxisInfo(std::string key, AxisType typeFlags = tags::UnknownAxisType, 
+             double resolution = 0.0, std::string description = "")
+    : key_(key),
+      description_(description),
+      resolution_(resolution),
+      flags_(typeFlags)
+    {}
+    
+    std::string key() const
+    {
+        return key_;
+    }
+    
+    std::string description() const
+    {
+        return description_;
+    }
+    
+    void setDescription(std::string const & description)
+    {
+        description_ = description;
+    }
+    
+    double resolution() const
+    {
+        return resolution_;
+    }
+    
+    void setResolution(double resolution)
+    {
+        resolution_ = resolution;
+    }
+    
+    AxisType typeFlags() const
+    {
+        return flags_ == 0
+                  ? tags::UnknownAxisType
+                  : flags_;
+    }
+    
+    bool isUnknown() const
+    {
+        return isType(tags::UnknownAxisType);
+    }
+    
+    bool isSpatial() const
+    {
+        return isType(tags::Space);
+    }
+    
+    bool isTemporal() const
+    {
+        return isType(tags::Time);
+    }
+    
+    bool isChannel() const
+    {
+        return isType(tags::Channels);
+    }
+    
+    bool isFrequency() const
+    {
+        return isType(tags::Frequency);
+    }
+
+    bool isEdge() const
+    {
+        return isType(tags::Edge);
+    }
+    
+    bool isAngular() const
+    {
+        return isType(tags::Angle);
+    }
+    
+    bool isType(AxisType type) const
+    {
+        return (typeFlags() & type) != 0;
+    }
+    
+    std::string repr() const
+    {
+        std::string res("AxisInfo: '");
+        res += key() + "' (type:";
+        if(isUnknown())
+        {
+            res += " none";
+        }
+        else
+        {
+            if(isChannel())
+                res += " Channels";
+            if(isSpatial())
+                res += " Space";
+            if(isTemporal())
+                res += " Time";
+            if(isEdge())
+                res += " Edge";
+            if(isAngular())
+                res += " Angle";
+            if(isFrequency())
+                res += " Frequency";
+        }
+        if(resolution_ > 0.0)
+        {
+            res += ", resolution=";
+            res += std::to_string(resolution_);
+        }
+        res += ")";
+        if(description_ != "")
+        {
+            res += " ";
+            res += description_;
+        }
+        return res;
+    }
+    
+    AxisInfo toFrequencyDomain(unsigned int size = 0, int sign = 1) const
+    {
+        AxisType type;
+        if(sign == 1)
+        {
+            vigra_precondition(!isFrequency(),
+                "AxisInfo::toFrequencyDomain(): axis is already in the Fourier domain.");
+            type = AxisType(tags::Frequency | flags_);
+        }
+        else
+        {
+            vigra_precondition(isFrequency(),
+                "AxisInfo::fromFrequencyDomain(): axis is not in the Fourier domain.");
+            type = AxisType(~tags::Frequency & flags_);
+        }
+        AxisInfo res(key_, type, 0.0, description_);
+        if(resolution_ > 0.0 && size > 0u)
+            res.resolution_ = 1.0 / (resolution_ * size);
+        return res;
+    }
+    
+    AxisInfo fromFrequencyDomain(unsigned int size = 0) const
+    {
+        return toFrequencyDomain(size, -1);
+    }
+
+    bool compatible(AxisInfo const & other) const
+    {
+        return isUnknown() || other.isUnknown() || 
+               ((typeFlags() & ~tags::Frequency) == (other.typeFlags() & ~tags::Frequency) &&
+                 key() == other.key());
+    }
+
+    bool operator==(AxisInfo const & other) const
+    {
+        return typeFlags() == other.typeFlags() && key() == other.key();
+    }
+
+    bool operator!=(AxisInfo const & other) const
+    {
+        return !operator==(other);
+    }
+    
+    // the primary ordering is according to axis type:
+    //     Channels < Space < Angle < Time < Frequency < Edge < Unknown
+    // the secondary ordering is the lexicographic ordering of the keys
+    //     "x" < "y" < "z"
+    bool operator<(AxisInfo const & other) const
+    {
+        return (typeFlags() < other.typeFlags()) || 
+                (typeFlags() == other.typeFlags() && key() < other.key());
+    }
+
+    bool operator<=(AxisInfo const & other) const
+    {
+        return !(other < *this);
+    }
+    
+    bool operator>(AxisInfo const & other) const
+    {
+        return other < *this;
+    }
+    
+    bool operator>=(AxisInfo const & other) const
+    {
+        return !(*this < other);
+    }
+    
+    std::string key_, description_;
+    double resolution_;
+    AxisType flags_;
+};
+
+inline
+std::string to_string(AxisInfo const & i)
+{
+    return i.repr();
+}
+
+inline
+std::ostream & operator<<(std::ostream & o, AxisInfo const & i)
+{
+    o << to_string(i);
+    return o;
+}
+
+// template <int N>
+// struct DefaultAxisTags
+// {
+    // static TinyArray<AxisTag, N> exec()
+    // {
+        // using namespace tags;
+        // static const AxisTag std[] = { t, z, y, x, c };
+    // }
+// };
+
+} // namespace vigra
+
+#endif // VIGRA_AXISTAGS_HXX
---
+++ test/test_axistags.cxx
@@ -0,0 +1,106 @@
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 2014-2015 by Ullrich Koethe                  */
+/*                                                                      */
+/*    This file is part of the VIGRA2 computer vision library.          */
+/*    The VIGRA2 Website is                                             */
+/*        http://ukoethe.github.io/vigra2                               */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        ullrich.koethe@iwr.uni-heidelberg.de    or                    */
+/*        vigra@informatik.uni-hamburg.de                               */
+/*                                                                      */
+/*    Permission is hereby granted, free of charge, to any person       */
+/*    obtaining a copy of this software and associated documentation    */
+/*    files (the "Software"), to deal in the Software without           */
+/*    restriction, including without limitation the rights to use,      */
+/*    copy, modify, merge, publish, distribute, sublicense, and/or      */
+/*    sell copies of the Software, and to permit persons to whom the    */
+/*    Software is furnished to do so, subject to the following          */
+/*    conditions:                                                       */
+/*                                                                      */
+/*    The above copyright notice and this permission notice shall be    */
+/*    included in all copies or substantial portions of the             */
+/*    Software.                                                         */
+/*                                                                      */
+/*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND    */
+/*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES   */
+/*    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND          */
+/*    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT       */
+/*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
+/*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
+/*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */
+/*                                                                      */
+/************************************************************************/
+
+#include <typeinfo>
+#include <iostream>
+#include <string>
+#include <vigra2/unittest.hxx>
+#include <vigra2/axistags.hxx>
+
+using namespace vigra;
+
+struct AxisTagsTest
+{
+    AxisTagsTest()
+    {
+    }
+    
+    void test()
+    {
+        using namespace tags;
+        shouldEqual(to_string(AxisInfo(x)),  std::string("AxisInfo: 'x' (type: Space)"));
+        shouldEqual(to_string(AxisInfo(y)),  std::string("AxisInfo: 'y' (type: Space)"));
+        shouldEqual(to_string(AxisInfo(z)),  std::string("AxisInfo: 'z' (type: Space)"));
+        shouldEqual(to_string(AxisInfo(t)),  std::string("AxisInfo: 't' (type: Time)"));
+        shouldEqual(to_string(AxisInfo(c)),  std::string("AxisInfo: 'c' (type: Channels)"));
+        shouldEqual(to_string(AxisInfo(n)),  std::string("AxisInfo: 'n' (type: Space)"));
+        shouldEqual(to_string(AxisInfo(e)),  std::string("AxisInfo: 'e' (type: Edge)"));
+        shouldEqual(to_string(AxisInfo(fx)), std::string("AxisInfo: 'x' (type: Space Frequency)"));
+        shouldEqual(to_string(AxisInfo(fy)), std::string("AxisInfo: 'y' (type: Space Frequency)"));
+        shouldEqual(to_string(AxisInfo(fz)), std::string("AxisInfo: 'z' (type: Space Frequency)"));
+        shouldEqual(to_string(AxisInfo(ft)), std::string("AxisInfo: 't' (type: Time Frequency)"));
+        shouldEqual(to_string(AxisInfo()), std::string("AxisInfo: '?' (type: none)"));
+        
+        AxisTag sorted[] = { c, n, x, y, z, t, fx, fy, fz, ft, e, unknown_axistag};
+        
+        for(int i=0; i<11; ++i)
+            should(AxisInfo(sorted[i]) < AxisInfo(sorted[i+1]));
+    }
+    
+    void testException()
+    {
+        try
+        {
+            failTest("no exception thrown");
+        }
+        catch(std::runtime_error & e)
+        {
+            std::string expected("expected message");
+            std::string message(e.what());
+            should(0 == expected.compare(message.substr(0,expected.size())));
+        }
+    }
+};
+
+struct AxisTagsTestSuite
+: public vigra::test_suite
+{
+    AxisTagsTestSuite()
+    : vigra::test_suite("AxisTagsTestSuite")
+    {
+        add( testCase(&AxisTagsTest::test));
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    AxisTagsTestSuite test;
+
+    int failed = test.run(vigra::testsToBeExecuted(argc, argv));
+
+    std::cout << test.report() << std::endl;
+
+    return (failed != 0);
+}
